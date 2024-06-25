4f17c8c18f6dc4e153be5732ba9f7a0dfe76fe9e
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-06-25 13:20:04 +0200

    cars carousel added

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import Link from 'next/link'
-import Carousel from './carousel'
 import { IoPersonOutline } from "react-icons/io5";
+import { Raleway } from 'next/font/google';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import NextJsCarousel from './carousel';
+
+const raleway = Raleway({
+    weight: '800',
+    subsets: ['latin'],
+  })
+   
+  
+
+
 
 function Hero() {
   return (
     
-    <div class="hero" style={{ backgroundColor: 'black' }} >
+    <div class="hero"  style={{ backgroundColor: 'black' }} >
     <div class="container">
         <div class="row justify-content-between">
             <div class="col-lg-5">
                 <div class="intro-excerpt">
               
-                    <h1 >Japan Direct <div  style={{ color: 'red',fontFamily: 'Raleway',fontWeight: 'bold'}} >Automative</div> Parts</h1>
-                    <p style={{ color: '#002F63',}}  class="mb-4">
+                    <h1  className={raleway.className}>Japan Direct <div className={raleway.className}  style={{ color: 'red',fontFamily: 'Raleway'}} >Automative</div> Parts</h1>
+                    <p style={{ color: '#002F63',}} className={raleway.className}   class="mb-4">
                     Delivering automotive excellence through quality parts and exceptional service, continuously driving innovation and customer satisfaction in the industry.
 
 </p>
@@ -28,8 +39,8 @@ function Hero() {
                 </div>
             </div>
             <div class="col-lg-7">
-                <div class="hero-img-wrap">
-                    <img src="https://zpuplawsjodqxxfqxchz.supabase.co/storage/v1/object/public/Car%20parts/GD6.png" class="img-fluid"/>
+                <div>
+                    <NextJsCarousel/>
                 </div>
             </div>
         </div>
