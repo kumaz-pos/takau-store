5962768ddfec77e0ab88d1539bad54d211d3134a
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-06-25 11:45:17 +0200

    home page changes

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
+import Carousel from './carousel'
+import { IoPersonOutline } from "react-icons/io5";
+
 function Hero() {
   return (
     
@@ -8,8 +11,9 @@ function Hero() {
         <div class="row justify-content-between">
             <div class="col-lg-5">
                 <div class="intro-excerpt">
-                    <h1>Japan Direct Automative Parts</h1>
-                    <p class="mb-4">
+              
+                    <h1 >Japan Direct <div  style={{ color: 'red',fontFamily: 'Raleway',fontWeight: 'bold'}} >Automative</div> Parts</h1>
+                    <p style={{ color: '#002F63',}}  class="mb-4">
                     Delivering automotive excellence through quality parts and exceptional service, continuously driving innovation and customer satisfaction in the industry.
 
 </p>
@@ -18,13 +22,14 @@ function Hero() {
                         <Link href="/shop" className='btn-shop'>
                         Shop Now
                         </Link>
-                        <a href="#" class="btn btn-white-outline">Explore</a>
+                        
+                        <a href="#" style={{ backgroundColor: '#002F63' }}  class="btn btn-white-outline">Explore</a>
                         </p>
                 </div>
             </div>
             <div class="col-lg-7">
                 <div class="hero-img-wrap">
-                    <img src="https://zpuplawsjodqxxfqxchz.supabase.co/storage/v1/object/public/Car%20parts/Aqua.png?t=2024-06-24T10%3A42%3A03.078Z" class="img-fluid"/>
+                    <img src="https://zpuplawsjodqxxfqxchz.supabase.co/storage/v1/object/public/Car%20parts/GD6.png" class="img-fluid"/>
                 </div>
             </div>
         </div>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,16 +2,20 @@
 import React from 'react'
 import Link from 'next/link'
 import supabase from "@/helpers/supabase";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
 
 
 async function Navbar() {
     const data= await supabase.from("Cart").select("*");
   return (
+    
+    
     <nav   class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
         
     <div class="container" >
 
-        <Link class="navbar-brand" href="/"><img src="https://zpuplawsjodqxxfqxchz.supabase.co/storage/v1/object/public/Car%20parts/japan_direct_logo.png" height={100} style={{ opacity: '1' }} width={200} alt="Image" class="img-fluid"/><span>.</span></Link>
+        <Link class="navbar-brand" href="/"><img src="https://zpuplawsjodqxxfqxchz.supabase.co/storage/v1/object/public/Car%20parts/japan_direct_logo_w_background.png" height={100} style={{ opacity: '1' }} width={200} alt="Image" class="img-fluid"/><span></span></Link>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -26,19 +30,25 @@ async function Navbar() {
                
                 <li><Link class="nav-link" href="/shop">Shop</Link></li>
                 <li><Link class="nav-link" href="/orders">Orders</Link></li>
-               
+            
             
             </ul>
 
             <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-                <li><a class="nav-link" href="#"><img src="images/user.svg"/></a></li>
-                <li><Link class="nav-link" href="/cart"><img src="images/cart.svg"/> <sup>{data.data.length}
+            <li>
+                <a class="nav-link" href="#">
+                    <IoPersonOutline color='red' size={30} />
+                </a>
+            </li>
+             
+                <li><Link class="nav-link" href="/cart"><IoCartOutline color='red' size={30} /><sup  style={{ color: 'red'}}>{data.data.length}
                     </sup> </Link></li>
             </ul>
         </div>
     </div>
         
 </nav>
+
   )
 }
 
