978f8a3332589416802646f10622b7ff0edc71f8
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-06-25 16:20:19 +0200

    welcome page ready

--- components/product-section.jsx
+++ components/product-section.jsx
@@ -15,7 +15,7 @@ async function ProductSection() {
 
             <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
                 <h2 style={{ color: 'white' }} class="mb-4 section-title">Crafted with superior automotive-grade materials</h2>
-                <p class="mb-4" style={{ color: 'pink',lineHeightStep:'3' }}>
+                <p class="mb-4" style={{ color: 'white',lineHeightStep:'3' }}>
                 Japan Direct is a leading provider of high-quality car parts, dedicated to supplying the automotive industry with exceptional products and services. With a relentless focus on innovation and customer satisfaction, we strive to deliver solutions that exceed expectations. </p>
                 <p>  <button className='btn-shop'>
                         Shop Now
