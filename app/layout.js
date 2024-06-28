0fb63228469a1e2625c714ccc72344fda2ec4762
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-06-28 14:13:53 +0200

    taiwind working

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 
 import { Inter } from "next/font/google";
 import { global } from "styled-jsx/css";
-
+import './globals.css'
 import {SessionProvider} from "next-auth/react"
 const inter = Inter({ subsets: ["latin"] });
 
